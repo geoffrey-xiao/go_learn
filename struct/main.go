9406b30f2b9680aa1280f95b6eadff46f4afe507
fengfeng <1377499035@qq.com>
2023-10-21 14:57:53 +0800

    struct & pointer

---
+++ struct/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contact   contactInfo
+}
+
+func main() {
+	// geoffrey := person{
+	// 	firstName: "Geoffrey",
+	// 	lastName:  "Xiao",
+	// }
+	// var geoffrey person
+	// geoffrey.firstName = "Geoffrey"
+	// geoffrey.lastName = "Xiao"
+	geoffrey := person{
+		firstName: "Geoffrey",
+		lastName:  "Xiao",
+		contact: contactInfo{
+			email:   "geoffrey@gmail.com",
+			zipCode: 0000,
+		},
+	}
+	// fmt.Println(geoffrey)
+	// fmt.Printf("%+v", geoffrey)
+	geoffreyPointer := &geoffrey
+	geoffreyPointer.updateName("jeff")
+	geoffrey.print()
+}
+
+func (pointerToPerson *person) updateName(newName string) {
+	(*pointerToPerson).firstName = newName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
